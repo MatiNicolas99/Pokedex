2e75e614e66a1b7e78b43d5c2a214393f52bc15e
MatiNicolas99 <103612186+MatiNicolas99@users.noreply.github.com>
2022-06-23 21:18:36 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let contenedor = document.querySelector('.contenedor');
+
+document.addEventListener('DOMContentLoaded', () => {
+    function fecthPokemones(numero) {
+        for(let n = 1; n <= numero; n++)
+        fetchDatos(n);
+    }
+fecthPokemones(20); 
+});
+
+
+const fetchDatos = async (id) => {
+    try {
+        const fetch_datos = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const data = await fetch_datos.json()
+        creaPokemones(data);
+        console.log(data)
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const creaPokemones = (pokemon) => {
+    const card = document.createElement('div');
+    card.classList.add('pokemon-card');
+
+    const contenedorImagen = document.createElement('div');
+    contenedorImagen.classList.add('img-container');
+
+    const fotoPokemon = document.createElement('img');
+    fotoPokemon.src = pokemon.sprites.front_default;
+
+    contenedorImagen.appendChild(fotoPokemon);
+
+    const numero_pokemon = document.createElement('p');
+    numero_pokemon.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+
+    const name = document.createElement('p');
+    name.classList.add('name-pokemon')
+    name.textContent = pokemon.name;
+
+    card.appendChild(contenedorImagen);
+    card.appendChild(numero_pokemon);
+    card.appendChild(name);
+
+    contenedor.appendChild(card);
+
+}
